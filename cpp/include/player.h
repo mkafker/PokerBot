da5972dc1bdfbf2bd279f1d0873959ae40bdcf04
Mikael <mikovtun@uw.edu>
2024-03-17 17:11:05 -0700

    m

--- cpp/include/player.h
+++ cpp/include/player.h
@@ -104,33 +104,6 @@ namespace Poker{
         return ret;
     };
 
-    struct Strategy {
-        public:
-            virtual PlayerMove makeMove(const std::shared_ptr<Table>, const shared_ptr<Player>);
-    };
-    class RandomAI : public Strategy {
-        public:
-            // inherit the constructors from the Strategy class
-            using Strategy::Strategy;
-            PlayerMove makeMove(std::shared_ptr<Table> info, const shared_ptr<Player>) override;
-    };
-    class SingleMoveCallAI : public Strategy {
-        public:
-            using Strategy::Strategy;
-            PlayerMove makeMove(std::shared_ptr<Table> info, const shared_ptr<Player>) override;
-    };
-    class SequenceMoveAI : public Strategy {
-        // Strategy instance that follows a sequence of moves
-        public:
-            std::vector<PlayerMove> moveList;
-            size_t index;
-            SequenceMoveAI() : Strategy()  {
-                index = 0;
-                moveList = vector<PlayerMove>();
-            }
-            PlayerMove makeMove(std::shared_ptr<Table> info, const shared_ptr<Player>) override;
-    };
-
     class Player {
         public:
             int  bankroll;
