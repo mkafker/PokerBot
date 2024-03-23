77730b5ad5c777552bdcbfb29098a0bb5579ae62
mkafker <kafkem@uw.edu>
2024-03-22 17:42:05 -0700

    Add shark bot

--- matt/poker2.py
+++ matt/poker2.py
@@ -1,4 +1,7 @@
 from pypokerengine.players import BasePokerPlayer
+from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+from pypokerengine.api.game import setup_config, start_poker
+import numpy as np
 
 
 # Fish player always calls
@@ -26,7 +29,7 @@ class FishPlayer(BasePokerPlayer):  # Do not forget to make parent class as "Bas
     def receive_round_result_message(self, winners, hand_info, round_state):
         pass
 
-from pypokerengine.utils.card_utils import gen_cards, estimate_hole_card_win_rate
+
 
 NB_SIMULATION = 1000
 
@@ -62,7 +65,74 @@ class HonestPlayer(BasePokerPlayer):
     def receive_round_result_message(self, winners, hand_info, round_state):
         pass
 
-from pypokerengine.api.game import setup_config, start_poker
+
+class SharkPlayer(BasePokerPlayer):
+
+    def declare_action(self, valid_actions, hole_card, round_state):
+        community_card = round_state['community_card']
+        win_rate = estimate_hole_card_win_rate(
+            nb_simulation=NB_SIMULATION,
+            nb_player=self.nb_player,
+            hole_card=gen_cards(hole_card),
+            community_card=gen_cards(community_card)
+        )
+        
+        # Customize these thresholds as needed
+        fold_threshold = 1.0 / self.nb_player
+        call_threshold = 0.5
+        raise_threshold = 0.7
+        
+        if win_rate < fold_threshold:
+            action = valid_actions[0]  # fold
+        elif win_rate < call_threshold:
+            action = valid_actions[1]  # call
+        elif win_rate < raise_threshold:
+            # Moderate confidence, might still want to call to see the next card
+            action = valid_actions[1]  # call
+        else:
+            # action = valid_actions[0]  # fold ####### TEMP
+
+
+            # High confidence, try to raise
+            # Check if raising is a valid action
+            # [print(act) for act in valid_actions]
+            # assert False
+            raise_action = [act for act in valid_actions if act['action'] == 'raise']
+            print(raise_action)
+            if raise_action:
+                action = raise_action[0]
+                # action['amount'] = int(raise_action[0]['amount']['min'] + np.random.rand()*(raise_action[0]['amount']['max'] - raise_action[0]['amount']['min'])//1)
+                action['amount'] = raise_action[0]['amount']['max']
+                print(action['amount'])
+                # action['amount'] = 15 # This works
+
+            else:
+                action = valid_actions[1]  # default to call if raise is not available
+
+            # Appears to work
+            # action = valid_actions[2] 
+            # action['amount'] = 15
+
+
+        return action['action'], action['amount']
+
+    def receive_game_start_message(self, game_info):
+        self.nb_player = game_info['player_num']
+
+    def receive_round_start_message(self, round_count, hole_card, seats):
+        pass
+
+    def receive_street_start_message(self, street, round_state):
+        pass
+
+    def receive_game_update_message(self, action, round_state):
+        pass
+
+    def receive_round_result_message(self, winners, hand_info, round_state):
+        pass
+
+    # Implement other required methods from BasePokerPlayer...
+
 
 #config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
 #config.register_player(name="p1", algorithm=FishPlayer())
@@ -70,10 +140,13 @@ from pypokerengine.api.game import setup_config, start_poker
 #config.register_player(name="p3", algorithm=HonestPlayer())
 #game_result = start_poker(config, verbose=1)
 
-from pypokerengine.api.game import setup_config, start_poker
+
 config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
-config.register_player(name="fish_player", algorithm=FishPlayer())
-config.register_player(name="honest_player", algorithm=HonestPlayer())
-game_result = start_poker(config, verbose=1)
+config.register_player(name="f1", algorithm=FishPlayer())
+config.register_player(name="h1", algorithm=HonestPlayer())
+config.register_player(name="s1", algorithm=SharkPlayer())
+
+
+game_result = start_poker(config, verbose=0)
 for player_info in game_result["players"]:
     print(player_info)
