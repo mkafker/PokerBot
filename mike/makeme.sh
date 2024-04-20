0b15310cf3c51633f8391dcc9e2cc6d744cb3718
mkafker <kafkem@uw.edu>
2024-04-19 17:22:53 -0700

    Manually added standard to makeme.sh

--- mike/makeme.sh
+++ mike/makeme.sh
@@ -1,3 +1,3 @@
-g++ -I include $(python3 -m pybind11 --includes) -g  src/*.cpp -o a.out -O3 -march=native
+g++ -I include $(python3 -m pybind11 --includes) -g  src/*.cpp -o a.out -O3 -march=native -std=c++17
 #cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 
